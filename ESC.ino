a9f62b508b6c22a4ff2071fd621450c26bfcbe94
ciungucristian <ciungucristian99@gmail.com>
2021-03-28 22:53:15 -0400

    program can now control all 4 motors

--- ESC.ino
+++ ESC.ino
@@ -1,35 +1,52 @@
 // ESC motor program 
 // Authors: Cristian Ciungu, Oussama Mehdi 
-// Date: March 20th, 2021 
+// Date: March 28th, 2021 
 // ------------------------------------------ 
 
-// include Servo and Serial libraries 
+// include Servo library 
 #include <Servo.h>
-#include <Serial.h> 
 
 // PWM and pin parameters 
 int Delay = 1000;
 int maxSignal = 2000;
 int minSignal = 1000;
-int motorPin = 9;
+int MP1 = 9;
+int MP2 = 10;
+int MP3 = 11;
+int MP4 = 6;
 float Speed = 0; 
 
-// declare Servo object 
-Servo motor;
+// declare Servo objects 
+Servo motor1;
+Servo motor2;
+Servo motor3;
+Servo motor4;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Calibrating ESCs ...");
   delay(2500);
-  motor.attach(motorPin); // 
+  motor1.attach(MP1); 
+  motor2.attach(MP2); 
+  motor3.attach(MP3); 
+  motor4.attach(MP4); 
+
   Serial.println("Plug battery pack to the PDB, then wait 2 seconds and press any key:");
-  motor.writeMicroseconds(maxSignal); // write MAX and MIN to ESCs to disable safety mode, i.e. calibrate ESCs 
+  motor1.writeMicroseconds(maxSignal); // write MAX and MIN to ESCs to disable safety mode, i.e. calibrate ESCs 
+  motor2.writeMicroseconds(maxSignal);
+  motor3.writeMicroseconds(maxSignal);
+  motor4.writeMicroseconds(maxSignal);
 
   // Wait for input
   while (!Serial.available());
   Serial.read();
 
-  motor.writeMicroseconds(minSignal); // write MAX and MIN to ESCs to disable safety mode, i.e. calibrate ESCs 
+  motor1.writeMicroseconds(minSignal); // write MAX and MIN to ESCs to disable safety mode, i.e. calibrate ESCs 
+  motor2.writeMicroseconds(minSignal);
+  motor3.writeMicroseconds(minSignal);
+  motor4.writeMicroseconds(minSignal);
+
+  
   Serial.println("Now, type a value between 1000 and 2000 and press enter:");
   Serial.println("The motors will start rotating.");
   Serial.println("1000: motor stops, 2000: full throttle");
@@ -40,7 +57,11 @@ void loop() {
   if (Serial.available() > 0){       // wait for user input
     Delay = Serial.parseInt(); 
     if (Delay> 999){ 
-      motor.writeMicroseconds(Delay); // motor runs 
+      motor1.writeMicroseconds(Delay); // 4 motors run 
+      motor2.writeMicroseconds(Delay);
+      motor3.writeMicroseconds(Delay);
+      motor4.writeMicroseconds(Delay);
+
       float Speed = (Delay-1000)/10;  // speed percentage displayed on Serial monitor 
       Serial.println("Motor speed:"); 
       Serial.print("  "); 
